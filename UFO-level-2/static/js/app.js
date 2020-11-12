f17ca1ce48b28a80e2f380fbea3bab3631b7f95b
Justin "Judd" Fowler <65981023+GitJudd@users.noreply.github.com>
2020-11-12 14:38:11 -0600

    Add files via upload
    Added to base CSS, combined both UFO-level-1 and UFO-level-2 into one by using a function that filters all terms in an object (table-area).

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,14 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+tableData.forEach(function(ufoData) {
+	    var row = tbody.append("tr");
+	Object.entries(ufoData).forEach(function([key, value]) {
+		var cell = row.append("td");
+		cell.text(value);
+    });
+  });
+
